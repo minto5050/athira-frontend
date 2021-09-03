453e46da6706cc420448b7e98feeccbbae736e01
Minto Issac <minto@mobilityeye.com>
2021-09-03 17:21:58 +0400

    initial commmit

---
+++ js/nw-ops.js
@@ -0,0 +1,19 @@
+function APIClient(endpoint) {
+    this.url = endpoint;
+}
+APIClient.prototype = {
+    constructor: APIClient,
+	search:function(isbn){
+		
+	},
+	getAll:function(){
+		
+	},
+	get: function(){
+		
+	},
+	post: function(){
+		
+	}
+	
+};
